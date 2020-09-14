4f969807a256d0200fe72d01e83c7fa471be4795
zahratulrahmi <zahratulrahmi04@gmail.com>
2020-09-14 19:43:15 +0700

    First Commit

---
+++ Test Suites/Kalkulator.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Kalkulator</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fb42ed48-e81f-4522-80b2-c99a4a86d293</testSuiteGuid>
+   <testCaseLink>
+      <guid>2d786049-47e2-49b3-99d3-d990183af7c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kalkulator/TC_StartApp</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0a54f36-c446-4eb4-ab89-ae647fc007fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kalkulator/TC_Penjumlahan</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cbc40701-af0a-4a5b-9b0f-bd62f51688bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kalkulator/TC_Pengurangan</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0ab3d9a9-190f-46a7-876b-6831039c98a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kalkulator/TC_Perkalian</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e56acc9-8656-48d4-af5a-4bb2f25224c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kalkulator/TC_Pembagian</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>05ab80e9-d3f4-4311-98c6-b392ac797db9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kalkulator/TC_CloseApp</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
